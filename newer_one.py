7fe411c317c51a987d7ceb0c38898ab13731bbc4
Erik <erikpapp@external.mol.hu>
2023-12-16 20:17:33 +0000

    encoder

--- newer_one.py
+++ newer_one.py
@@ -32,15 +32,15 @@ MIN_COUNT = 1 # Keep any words from training data that show up equal to or more 
 
 # Configure models
 model_name = 'lex_llm' # For file saving label
-eval_interval = 100
-eval_iters = 200
+eval_interval = 10
+eval_iters = 5
 n_embed = 64
 n_head = 6
 n_layer = 6
 dropout = 0.2
 batch_size = 64
 block_size = 19
-max_iters = 3000
+max_iters = 30
 
 
 # Configure training/optimization
@@ -298,11 +298,91 @@ def estimate_loss():
         losses = torch.zeros(eval_iters)
         for k in range(eval_iters):
             Xin, Xout, Yin, Yout = get_batch(split)
-            logits, loss = model(Xout, Yout)
+            logits, loss = model(Xin, Xout, targets=Yout)
             losses[k] = loss.item()
         out[split] = losses.mean()
     model.train()
     return out
+
+
+class EncoderHead(nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embed, head_size, bias=False)
+        self.query = nn.Linear(n_embed, head_size, bias=False)
+        self.value = nn.Linear(n_embed, head_size, bias=False)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        B,T,C = x.shape
+        k = self.key(x) # (B,T,hs)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B,T,hs) @ (B,hs,T) -> (B,T,T)
+        wei = F.softmax(wei, dim=-1) # (B,T,T)
+        wei = self.dropout(wei)
+        #weighted aggregation of values
+        v = self.value(x) # (B,T,hs)
+        out = wei @ v # (B,T,T) @ (B,T,hs) -> (B,T,hs)
+        return out
+    
+class EncoderMultiHeadAttention(nn.Module):
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([EncoderHead(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embed)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
+
+
+
+
+class NoMaskHead(nn.Module):
+
+    def __init__(self, head_size):
+        super().__init__()
+        self.key = nn.Linear(n_embed, head_size, bias=False)
+        self.query = nn.Linear(n_embed, head_size, bias=False)
+        self.value = nn.Linear(n_embed, head_size, bias=False)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, enc_out):
+        # input of size (batch, time-step, channels)
+        # output of size (batch, time-step, head size)
+        B,T,C = x.shape
+        k = self.key(enc_out)
+        q = self.query(x) # (B,T,hs)
+        # compute attention scores
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B,T,hs) @ (B,hs,T) -> (B,T,T)
+        wei = F.softmax(wei, dim=-1) # (B,T,T)
+        wei = self.dropout(wei)
+        #weighted aggregation of values
+        v = self.value(enc_out)
+        out = wei @ v # (B,T,T) @ (B,T,hs) -> (B,T,hs)
+        return out
+    
+class NoMaskMultiHeadAttention(nn.Module):
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([NoMaskHead(head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(head_size * num_heads, n_embed)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x, enc_out):
+        out = torch.cat([h(x, enc_out) for h in self.heads], dim=-1)
+        out = self.dropout(self.proj(out))
+        return out
 
 
 
@@ -360,12 +440,12 @@ class FeedForward(nn.Module):
     def forward(self, x):
         return self.net(x)
     
-class Block(nn.Module):
-
-    def __init__(self, n_embed, n_head):
+class EncoderBlock(nn.Module):
+    
+    def __init__(self,n_embed, n_head):
         super().__init__()
         head_size = n_embed // n_head
-        self.sa = MultiHeadAttention(n_head, head_size)
+        self.sa = EncoderMultiHeadAttention(n_head, head_size)
         self.ffwd = FeedForward(n_embed)
         self.ln1 = nn.LayerNorm(n_embed)
         self.ln2 = nn.LayerNorm(n_embed)
@@ -375,12 +455,31 @@ class Block(nn.Module):
         x = x + self.ffwd(self.ln2(x))
         return x
     
+class Block(nn.Module):
+
+    def __init__(self, n_embed, n_head):
+        super().__init__()
+        head_size = n_embed // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ca = NoMaskMultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_embed)
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.lnc = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
+
+    def forward(self, x, enc_out):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ca(self.lnc(x), enc_out)
+        x = x + self.ffwd(self.ln2(x))
+        return x
+    
 class LangMod(nn.Module):
 
     def __init__(self):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, n_embed)
         self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.encoderblocks = nn.Sequential(*[EncoderBlock(n_embed, n_head=n_head) for _ in range(n_layer)])
         self.blocks = nn.Sequential(*[Block(n_embed, n_head=n_head) for _ in range(n_layer)])
         self.ln_f = nn.LayerNorm(n_embed) #can be RMS norm
         self.lm_head = nn.Linear(n_embed, vocab_size)
@@ -395,13 +494,20 @@ class LangMod(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-    def forward(self, idx, targets=None):
+    def forward(self, enc_out, idx, targets=None):
         B,T = idx.shape
+        Be, Te = enc_out.shape
+
+        tok_emb_inp = self.token_embedding_table(enc_out)
+        pos_emb_inp = self.position_embedding_table(torch.arange(Te, device=device))
+        x_input = tok_emb_inp + pos_emb_inp
 
         tok_emb = self.token_embedding_table(idx) #(B,T,C)
         pos_emb = self.position_embedding_table(torch.arange(T, device=device)) #(T,C)
         x = tok_emb + pos_emb # (B,T,C)
-        x = self.blocks(x) # (B,T,C)
+
+        x_enc = self.encoderblocks(x_input)
+        x = self.blocks(x, x_enc) # (B,T,C)
         x = self.ln_f(x) # (B,T,C)
         logits = self.lm_head(x) #(B,T,vocab_size)
 
@@ -415,10 +521,10 @@ class LangMod(nn.Module):
 
         return logits, loss
     
-    def generate(self, idx, max_new_tokens):
+    def generate(self, inp, idx, max_new_tokens):
         for _ in range((max_new_tokens)):
             idx_cond = idx[:, -block_size:]
-            logits, loss = self(idx_cond)
+            logits, loss = self(inp, idx_cond)
             logits = logits[:, -1, :] # last time step (B,C)
             probs = F.softmax(logits, dim=-1) # (B,C)
             idx_next = torch.multinomial(probs, num_samples=1) # (B,1)
@@ -442,7 +548,7 @@ for iter in range(max_iters):
 
     xbi, xbo, ybi, ybo = get_batch("train")
 
-    logits, loss = model(xbo, ybo)
+    logits, loss = model(xbi, xbo, targets=ybo)
     optimizer.zero_grad(set_to_none=True)
     loss.backward()
     optimizer.step()
@@ -453,6 +559,7 @@ while on:
     if q == "quit":
         on = False
     else:
+        gen = torch.zeros((1,1), dtype=torch.long, device=device)
         context = torch.tensor([encoding.encode(q)], dtype=torch.long, device=device)
-        print(encoding.decode(m.generate(context, max_new_tokens=block_size)[0].tolist()))
+        print(encoding.decode(m.generate(context, gen, max_new_tokens=10)[0].tolist()))
 
